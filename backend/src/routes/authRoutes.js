3763e21a3424dc27315f5dd41deb1de429b7ca9c
Naveen Kumar M E <nks3244587@gmail.com>
2026-05-06 14:47:06 +0530

    Add quick sale route to authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const controller = require('../controllers/authController')
 const auth = require('../middleware/authMiddleware')
-
+router.post('/quick', auth, salesController.saveQuickSale); 
 router.post('/login', controller.login)
 router.get('/me', auth, controller.me)
 router.post('/change-password', auth, controller.changePassword)
