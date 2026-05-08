c823572cbe8fffbbceef0c194fc524f76b02875e
naveencmy <you@example.com>
2026-05-08 09:31:45 +0530

    Commit the Backend the cleared bug on the sales router misdirect

--- backend/src/routes/salesRoutes.js
+++ backend/src/routes/salesRoutes.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const router = express.Router()
-
 const salesController = require('../controllers/salesController')
 const auth = require('../middleware/authMiddleware')
-    */* Fix the path as needed to access the quick save*/
-router.post('/quick', auth, salesController.saveQuickSale)
-router.get('/quick', auth, salesController.getQuickSales)
-router.delete('/quick/:id', auth, salesController.deleteQuickSale)
-*/* Fix the path as needed to access the Sales mkodules*/
-router.post('/create', auth, salesController.createSale)
-router.post('/return', auth, salesController.returnSale)
-router.get('/:id', auth, salesController.getSaleById)
+/*
+QUICK SALES / DRAFTS
+*/
+router.post('/quick',auth,salesController.saveQuickSale)
+router.get('/quick',auth,salesController.getQuickSales)
+router.delete('/quick/:id',auth,salesController.deleteQuickSale)
+/*
+Sales
+*/
+router.post('/create',auth,salesController.createSale)
+router.post('/return',auth,salesController.returnSale)
+router.get('/:id',auth,salesController.getSaleById)
+
 module.exports = router
